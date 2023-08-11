dc689785396359a8a09cc29438ec2ecd17375590
annsilva <annmargaret.silva@gmail.com>
2023-08-11 19:09:58 -0400

    changes for environment variables and deployment

---
+++ config.py
@@ -0,0 +1,7 @@
+"""Flask App configuration."""
+
+# General Config
+ENVIRONMENT = "development"
+SECRET_KEY='s3cr3tk3yN0SQLPr0j3ct'
+DATABASE_URI='mongodb+srv://ann:root@fitnesscluster.jgxt2re.mongodb.net/'
+DEBUG=True
